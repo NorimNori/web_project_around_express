141f642b235a8dffaeab0cca96d0c7445405c1cf
Gloria R <norimaqidoe@hotmail.com>
2026-02-14 01:38:43 -0600

    feat: create users model

---
+++ models/users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const urlRegex =
+  /^https?:\/\/(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?#?$/;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return urlRegex.test(value);
+      },
+      message: "La URL del avatar no es válido",
+    },
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
