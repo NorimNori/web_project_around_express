56f12cc53f9f1f76798be495b8a963cc8e48a43a
Gloria R <norimaqidoe@hotmail.com>
2026-02-14 01:26:53 -0600

    feat: connect db to app

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/aroundb");
 
 const cardsRouter = require("./routes/cards");
 const usersRouter = require("./routes/users");
