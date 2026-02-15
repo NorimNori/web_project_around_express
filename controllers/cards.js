42186727cc432dfd730652f6e7367ba49edbfdeb
Gloria R <norimaqidoe@hotmail.com>
2026-02-14 20:34:26 -0600

    feat: create users controllers and new routes

---
+++ controllers/cards.js
@@ -0,0 +1,57 @@
+const Card = require("../models/cards");
+
+const getAllCards = (req, res) => {
+  Card.find({})
+    .then((cards) => {
+      res.status(200).json(cards);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Error interno del servidor",
+      });
+    });
+};
+
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  const owner = "507f1f77bcf86cd799439011";
+
+  Card.create({ name, link, owner })
+    .then((card) => {
+      res.status(201).json(card);
+    })
+    .catch(() => {
+      res.status(400).json({
+        message: "Datos inválidos",
+      });
+    });
+};
+
+const deleteCard = (req, res) => {
+  const { id } = req.params;
+
+  Card.findByIdAndDelete(id)
+    .then((card) => {
+      if (!card) {
+        return res.status(404).json({
+          message: "Tarjeta no encontrada",
+        });
+      }
+
+      res.status(200).json({
+        message: "Tarjeta eliminada correctamente",
+      });
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Error interno del servidor",
+      });
+    });
+};
+
+module.exports = {
+  getAllCards,
+  createCard,
+  deleteCard,
+};
--- routes/cards.js
+++ routes/cards.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const fs = require("fs").promises;
-const path = require("path");
+
+const { getAllCards, createCard, deleteCard } = require("../controllers/cards");
 
 const router = express.Router();
 
-const cardsPath = path.join(__dirname, "..", "data", "cards.json");
+router.get("/", getAllCards);
 
-router.get("/", async (req, res) => {
-  try {
-    const data = await fs.readFile(cardsPath, { encoding: "utf8" });
-    const cards = JSON.parse(data);
-    res.json(cards);
-  } catch (error) {
-    res.status(500).json({
-      message: "Error al leer los datos de tarjetas",
-    });
-  }
-});
+router.post("/", createCard);
+
+router.delete("/:id", deleteCard);
 
 module.exports = router;
