72a7bb5aefe8339e7f593de82c07788c49271a67
Gloria R <norimaqidoe@hotmail.com>
2026-02-14 21:33:14 -0600

    feat: add error handling to user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,8 @@ const getAllUsers = (req, res) => {
     .then((users) => {
       res.status(200).json(users);
     })
-    .catch(() => {
-      res.status(500).json({
-        message: "Error interno del servidor",
-      });
+    .catch((err) => {
+      res.status(500).json({ message: "Error interno del servidor" });
     });
 };
 
@@ -16,16 +14,23 @@ const getUser = (req, res) => {
   const { id } = req.params;
 
   User.findById(id)
+    .orFail()
     .then((user) => {
-      if (!user) {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
         return res.status(404).json({
           message: "Usuario no encontrado",
         });
       }
 
-      res.status(200).json(user);
-    })
-    .catch(() => {
+      if (err.name === "CastError") {
+        return res.status(400).json({
+          message: "ID inválido",
+        });
+      }
+
       res.status(500).json({
         message: "Error interno del servidor",
       });
@@ -40,8 +45,14 @@ const createUser = (req, res) => {
       res.status(201).json(user);
     })
     .catch((err) => {
-      res.status(400).json({
-        message: "Datos inválidos",
+      if (err.name === "ValidationError") {
+        return res.status(400).json({
+          message: "Datos inválidos",
+        });
+      }
+
+      res.status(500).json({
+        message: "Error interno del servidor",
       });
     });
 };
