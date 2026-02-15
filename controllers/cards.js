9dd8b9ea212088537f15ba6362ef03a44c3d7dcb
Gloria R <norimaqidoe@hotmail.com>
2026-02-14 21:46:39 -0600

    feat: add error handling to cards controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -14,16 +14,21 @@ const getAllCards = (req, res) => {
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
-
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
     .then((card) => {
       res.status(201).json(card);
     })
-    .catch(() => {
-      res.status(400).json({
-        message: "Datos inválidos",
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).json({
+          message: "Datos inválidos",
+        });
+      }
+
+      res.status(500).json({
+        message: "Error interno del servidor",
       });
     });
 };
@@ -32,18 +37,25 @@ const deleteCard = (req, res) => {
   const { id } = req.params;
 
   Card.findByIdAndDelete(id)
-    .then((card) => {
-      if (!card) {
+    .orFail()
+    .then(() => {
+      res.status(200).json({
+        message: "Tarjeta eliminada correctamente",
+      });
+    })
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
         return res.status(404).json({
           message: "Tarjeta no encontrada",
         });
       }
 
-      res.status(200).json({
-        message: "Tarjeta eliminada correctamente",
-      });
-    })
-    .catch(() => {
+      if (err.name === "CastError") {
+        return res.status(400).json({
+          message: "ID inválido",
+        });
+      }
+
       res.status(500).json({
         message: "Error interno del servidor",
       });
